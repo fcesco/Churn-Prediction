4474ce8fff21ddade2929ef89a54540ff7c21376
Francesco Ciuci <fcesco>
2016-03-23 18:08:37 -0700

    some cleaning

--- churn.py
+++ churn.py
@@ -2,20 +2,27 @@ from datetime import timedelta
 import pandas as pd
 import numpy as np
 import seaborn as sns
-#import matplotlib.pyplot as plt
 from datetime import datetime
 import statsmodels.api as sm
-#plt.style.use('ggplot')
 import statsmodels.api as sm
 import psycopg2 as pg2
 import pandas.io.sql as pdsql
 from statsmodels import tsa
 from os import walk
 import operator
-# from sklearn.linear_model import LogisticRegression
 from unbalanced_dataset import SMOTE
 from scipy.stats import randint as sp_randint
+from sklearn import cross_validation
 
+
+impression_column_name = ['time', 'distinct_id', 'app_release', 'app_version', 'carrier', 'city', 'ios_ifa', 'lib_version', 'manufacturer', 'model', 'name', 'os', 'os_version', 'radio', 'region',  'screen_height', 'screen_width', 'wifi', 'account_status', 'audio', 'broadcaster_id', 'broadcaster_name', 'candies_sent', 'comments_sent', 'facebook_post_id', 'mode', 'page',  'public', 'screen', 'stream_duration', 'stream_id', 'stream_message', 'type', 'video', 'views_on_join', 'facebook_id', 'gender', 'locale', 'mp_country_code', 'mp_device_model', 'mp_lib', 'elapsed', 'app_version', 'screen_dpi', 'brand']
+
+stream_drop = ['_id', 'source_ip', 'page', 'created_at', 'title', 'text', 'message', 'friends', 'place', 'secret', 'base_uri', 'media_ring', 'bucket', 'video', 'audio', 'live', 'closed', 'stop_live_count', 'play_live_time', 'stop_vod_count', 'play_vod_time', 'size', 'live_at', 'origin_id', 'origin_ip', 'live_confirm_time', 'facebook_post_time', 'facebook_post_id', 'offline_at', 'closed_at', 'id', 'name', 'type', 'public', 'likes_views', 'time', 'short_url', 'device_locale', 'file_name']
+
+user_drop = ['enabled', 'last_ip', 'latitude', 'longitude', 'fb_name', 'fb_email', 'fb_access_token', 'fb_extended_access_token', 'email', 'fb_expires_at', 'fb_issued_at', 'user_friends', 'latest_page', 'languages', 'device_locale', 'public_profile', 'secure_browsing','type', 'allowed_viewers', 'facebook_id', 'created_at','bucket','fb_locale']
+
+impression_drop=['time','distinct_id','app_release','carrier','city','ios_ifa','lib_version','manufacturer','model','name','os',
+'os_version','radio','region','screen_height','screen_width','wifi','account_status','audio','broadcaster_id','broadcaster_name','facebook_post_id','page','public','screen','stream_message','type','video','facebook_id','locale','mp_country_code','mp_device_model','mp_lib','elapsed','app_version','brand']
 
 def drop_features(stream_table, user_table):
     '''
@@ -24,9 +31,6 @@ def drop_features(stream_table, user_table):
         user_table: table with user information
         stream_table: stream with streams information
     '''
-
-    stream_drop = ['_id', 'source_ip', 'page', 'created_at', 'title', 'text', 'message', 'friends', 'place', 'secret', 'base_uri', 'media_ring', 'bucket', 'video', 'audio', 'live', 'closed', 'stop_live_count', 'play_live_time', 'stop_vod_count', 'play_vod_time', 'size', 'live_at', 'origin_id', 'origin_ip', 'live_confirm_time', 'facebook_post_time', 'facebook_post_id', 'offline_at', 'closed_at', 'id', 'name', 'type', 'public', 'likes_views', 'time', 'short_url', 'device_locale', 'file_name']
-    user_drop = ['enabled', 'last_ip', 'latitude', 'longitude', 'fb_name', 'fb_email', 'fb_access_token', 'fb_extended_access_token', 'email', 'fb_expires_at', 'fb_issued_at', 'user_friends', 'latest_page', 'languages', 'device_locale', 'public_profile', 'secure_browsing','type', 'allowed_viewers', 'facebook_id', 'created_at','bucket','fb_locale']
     user_table = user_table[user_table['type']=='user']
     user_table.drop(user_drop, axis=1, inplace=True)
     stream_table.drop(stream_drop, axis=1, inplace=True)
@@ -40,8 +44,6 @@ def drop_impression(impression):
     Return:
         impression table without not desired columns
     '''
-
-    drop_list = ['time','distinct_id','app_release','carrier','city','ios_ifa','lib_version','manufacturer','model','name','os','os_version','radio','region','screen_height','screen_width','wifi','account_status','audio','broadcaster_id','broadcaster_name','facebook_post_id','page','public','screen','stream_message','type','video','facebook_id','locale','mp_country_code','mp_device_model','mp_lib','elapsed','app_version','brand']
     impression.drop(drop_list, axis=1, inplace=True)
     return impression
 
@@ -67,16 +69,12 @@ def create_churn(df, limit):
 
 def clean_user(df):
     '''
-    Clean the dataframe (user) in order to make it ready for the machine learning algorithm.
+    Clean the user dataframe
     '''
 
     df = df[df['last_login'].notnull()]
-    # df['created_at'] = user_table['created_at'].map(lambda x: (x-np.datetime64('1970-01-01T00:00:00Z'))/np.timedelta64(1, 's'))
     df['publish_actions'] = df['publish_actions'].replace([None],[-1])
     df['publish_actions'] = df['publish_actions'].astype(int)
-    #pd.to_datetime(df['last_login']
-    #df['last_login'] = df[['last_login']==np.datetime]
-    #df['last_login'] = df.apply(lambda x: datetime.utcfromtimestamp(x['last_login'].tolist()/1e9))
     df = pd.get_dummies(df, columns = ['gender'])
     df = integerize(df, 'manage_pages')
     df = integerize(df, 'valid_token')
@@ -128,14 +126,13 @@ def impression_user_merging(impressions_folder, df):
     Return:
         df table with the df
     '''
-    column = ['time', 'distinct_id', 'app_release', 'app_version', 'carrier', 'city', 'ios_ifa', 'lib_version', 'manufacturer', 'model', 'name', 'os', 'os_version', 'radio', 'region',  'screen_height', 'screen_width', 'wifi', 'account_status', 'audio', 'broadcaster_id', 'broadcaster_name', 'candies_sent', 'comments_sent', 'facebook_post_id', 'mode', 'page',  'public', 'screen', 'stream_duration', 'stream_id', 'stream_message', 'type', 'video', 'views_on_join', 'facebook_id', 'gender', 'locale', 'mp_country_code', 'mp_device_model', 'mp_lib', 'elapsed', 'app_version', 'screen_dpi', 'brand']
 
     total_candies_sent = pd.Series(name = 'candies_sent')
     total_comments_sent = pd.Series(name ='comments_sent')
     for (dirpath, dirnames, filenames) in walk(impressions_folder):
         filenames = [impressions_folder+filename for filename in filenames if not filename[0]=='.']
         for filename in filenames:
-            impression = pd.read_table(filename, engine='python', header=None, names = column)
+            impression = pd.read_table(filename, engine='python', header=None, names = impression_column_name)
             temp_candies_sent = impression.groupby(by='distinct_id')['candies_sent'].sum()
             temp_comments_sent = impression.groupby(by='distinct_id')['comments_sent'].sum()
             total_candies_sent = total_candies_sent.add(temp_candies_sent, fill_value=0)
@@ -173,11 +170,13 @@ def group_join(df_group, df_merge, by_column, column_group, merge_on, func):
 def integerize_join(df, column, df_merge):
     '''
     Change boolean values of dataframe column to integers replacing null values to -1.
+    Join the result to df_merge
     Args:
         df: Pandas dataframe
         column: Column to integerize
+        df_merge: Pandas dataframe to merge with column
     Return:
-        df: Dataframe
+        df_merge: return the merged dataframe
     '''
 
     temp = df[column].replace([None], [-1], regex=True )
@@ -188,6 +187,15 @@ def integerize_join(df, column, df_merge):
     return df_merge
 
 def integerize(df, column):
+    '''
+    Change boolean values of dataframe column to integers replacing null values to -1.
+    Args:
+        df: Pandas dataframe
+        column: Column to integerize
+    Return:
+        df_merge: return the merged dataframe
+    '''
+
     df[column] = df[column].replace([None],[-1],regex=True)
     df[column] = df[column].astype(int)
     return df
@@ -210,48 +218,46 @@ def import_data():
     return streams, users
 
 
-def create_node_value(churn_prob, user_id):
-    churn_prob = pd.DataFrame(data = {'user_id': user_id , 'churn_prob': churn_prob})
-    return churn_prob
-
-
-def weighted_edges(edges_file, impressions_folder, user):
-    '''
-    create weighted edges
-    '''
-    column = ['time', 'distinct_id', 'app_release', 'app_version', 'carrier', 'city', 'ios_ifa', 'lib_version', 'manufacturer', 'model', 'name', 'os', 'os_version', 'radio', 'region',  'screen_height', 'screen_width', 'wifi', 'account_status', 'audio', 'broadcaster_id', 'broadcaster_name', 'candies_sent', 'comments_sent', 'facebook_post_id', 'mode', 'page',  'public', 'screen', 'stream_duration', 'stream_id', 'stream_message', 'type', 'video', 'views_on_join', 'facebook_id', 'gender', 'locale', 'mp_country_code', 'mp_device_model', 'mp_lib', 'elapsed', 'app_version', 'screen_dpi', 'brand']
-    edges = pd.read_csv(edges_file)
-    edges = edges[edges.exists==True]
-    edges.insert(0, 'weight',1)
-    edges = edges[(edges.follower.isin(user.facebook_id.values)) & (edges.followed.isin(user.facebook_id.values))]
-    for (dirpath, dirnames, filenames) in walk(impressions_folder):
-        filenames = [impressions_folder+filename for filename in filenames if not filename[0]=='.']
-        for filename in filenames:
-            impression = pd.read_table(filename, engine='python', header=None, names =column)
-            edges['weight'] += edges.apply(lambda x: weight_edge(x, impression, user), axis = 1)
-    return edges
-
-
-def weight_edge(x, impression, user):
-    if any((impression['broadcaster_id'].isin(user[x['followed']==user['facebook_id']]['_id'])) & (impression['distinct_id'].isin(user[x['follower']==user['facebook_id']]['_id']))):
-        return 1
-    else: return 0
+# def create_node_value(churn_prob, user_id):
+#     churn_prob = pd.DataFrame(data = {'user_id': user_id , 'churn_prob': churn_prob})
+#     return churn_prob
 #
 #
-# def weight_edge(x, impression):
-#     if any((df1['value'] ==x['value']) & (df1['name'] == x['name'])):
+# def weighted_edges(edges_file, impressions_folder, user):
+#     '''
+#     create weighted edges
+#     '''
+#     column = ['time', 'distinct_id', 'app_release', 'app_version', 'carrier', 'city', 'ios_ifa', 'lib_version', 'manufacturer', 'model', 'name', 'os', 'os_version', 'radio', 'region',  'screen_height', 'screen_width', 'wifi', 'account_status', 'audio', 'broadcaster_id', 'broadcaster_name', 'candies_sent', 'comments_sent', 'facebook_post_id', 'mode', 'page',  'public', 'screen', 'stream_duration', 'stream_id', 'stream_message', 'type', 'video', 'views_on_join', 'facebook_id', 'gender', 'locale', 'mp_country_code', 'mp_device_model', 'mp_lib', 'elapsed', 'app_version', 'screen_dpi', 'brand']
+#     edges = pd.read_csv(edges_file)
+#     edges = edges[edges.exists==True]
+#     edges.insert(0, 'weight',1)
+#     edges = edges[(edges.follower.isin(user.facebook_id.values)) & (edges.followed.isin(user.facebook_id.values))]
+#     for (dirpath, dirnames, filenames) in walk(impressions_folder):
+#         filenames = [impressions_folder+filename for filename in filenames if not filename[0]=='.']
+#         for filename in filenames:
+#             impression = pd.read_table(filename, engine='python', header=None, names =column)
+#             edges['weight'] += edges.apply(lambda x: weight_edge(x, impression, user), axis = 1)
+#     return edges
+#
+#
+# def weight_edge(x, impression, user):
+#     if any((impression['broadcaster_id'].isin(user[x['followed']==user['facebook_id']]['_id'])) & (impression['distinct_id'].isin(user[x['follower']==user['facebook_id']]['_id']))):
 #         return 1
 #     else: return 0
 
-
-def find_follower(follower, edges):
-    pass
-
-
-
-#def user_graph
-
 def Xy_create(df, column_y, column_id):
+    '''
+    Return X and y ready for the model and user_id to keep track of the users will churn.
+    Args:
+        df: Pandas dataframe
+        column_y: column where there is endogenous variable
+        column_id: column where there are users id
+    Return:
+        X: exogenous variable
+        y: endogenous variable
+        user_id: series of the user id
+
+    '''
     y = df[column_y]
     df.drop(column_y, axis=1, inplace=True)
     user_id = df[column_id]
@@ -274,86 +280,38 @@ def smooting(X,y):
     smox, smoy = smote.fit_transform(X,y)
     return smox, smoy
 
-# def pre_modeling(df, column_y, column_id):
-#
-#     y = df[column_y]
-#     df.drop(column_y, axis=1,inplace = True)
-#     user_id = df[column_id]
-#     df.drop(column_id, axis=1, inplace=True)
-#     X = df.values
-#     X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.3, random_state=0)
-#     return X_train, X_test, y_train, y_test, column_id
-
-def score_model():
-    pass
-
-def nested_cross_validation(X,y):
-    outer_scores = []
-
-    # outer cross-validation
-    outer = cross_validation.KFold(len(y), n_folds=3, shuffle=True, random_state=state)
-    for fold, (train_index_outer, test_index_outer) in enumerate(outer):
-        X_train_outer, X_test_outer = X[train_index_outer], X[test_index_outer]
-        y_train_outer, y_test_outer = y[train_index_outer], y[test_index_outer]
-
-        inner_mean_scores = []
-        # define explored parameter space.
-        # procedure below should be equal to GridSearchCV
-        tuned_parameter = [1000, 1100, 1200]
-        for param in tuned_parameter:
-            scores = []
-            # normal cross-validation
-            kfolds = cross_validation.KFold(len(y), n_folds=3, shuffle=True, random_state=state)
-            for train_index, test_index in kfolds:
-                # split the training data
-                X_train, X_test = X[train_index], X[test_index]
-                y_train, y_test = y[train_index], y[test_index]
-
-                # fit extremely randomized trees regressor to training data
-                clf2_5 = ensemble.ExtraTreesRegressor(param, n_jobs=-1, random_state=1)
-                clf2_5.fit(X_train, y_train)
-                scores.append(clf2_5.score(X_test, y_test))
-
-            # calculate mean score for folds
-            mean_scores.append(np.mean(scores))
-        # get maximum score index
-        index, value = max(enumerate(mean_scores), key=operator.itemgetter(1))
-    print 'Best parameter : %i' % (tuned_parameter[index])
-
 if __name__=="__main__":
     impression_path = '/Users/Fra/Documents/Streamago/Streamago_churn_study/stream impression/'
     edges_path = '/Users/Fra/Documents/Streamago/Streamago_churn_study/graph/notify.csv'
     stream_table, user_table = import_data()
     strem_table, user_table = drop_features(stream_table, user_table)
     user_table = clean_user(user_table)
-    user_table = create_churn(user_table, 14) #possibly modify create_churn using a user usage decrease.
-    merged_data =stream_user_merging(stream_table, user_table)
+    user_table = create_churn(user_table, 14)
+    merged_data = stream_user_merging(stream_table, user_table)
     merged_data = impression_user_merging('/Users/Fra/Documents/Streamago/Streamago_churn_study/stream impression/', merged_data)
     merged_data = merged_data.fillna(-1)
-    X, y ,user_id = Xy_create(merged_data, 'churn', '_id')
+    X, y, user_id = Xy_create(merged_data, 'churn', '_id')
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.3, random_state=0)
+    smox, smoy = smooting(X_train_best, y_train)
 
-
-    smox, smoy = smooting(X_train_best, y)
-
-
-    rndF = ensemble.RandomForestClassifier()
-    score = cross_validation.cross_val_score(rndF, X_train, y_train)
-    rndF.fit(X_train,y_train)
-    rndF.score(X_test, y_test)
     #
-    param_dist = {"max_depth": [3, None],
-              "max_features": sp_randint(1, 11),
-              "min_samples_split": sp_randint(1, 11),
-              "min_samples_leaf": sp_randint(1, 11),
-              "bootstrap": [True, False],
-              "criterion": ["gini", "entropy"]}
-
-    n_iter_search = 20
-    random_search = RandomizedSearchCV(rndF, param_distributions=param_dist, n_iter=n_iter_search)
-    random_search.fit(X,y)
-    best_model = random_search.best_estimator_
-    best_features = best_model.feature_importances_.argsort()[::-1]
-    X_best = X[:,best_features[0:15]]
-    X_train_best = X_train[:, best_features[0:15]]
-    X_test_best = X_test[:, best_features[0:15]]
+    # rndF = ensemble.RandomForestClassifier()
+    # score = cross_validation.cross_val_score(rndF, X_train, y_train)
+    # rndF.fit(X_train,y_train)
+    # rndF.score(X_test, y_test)
+    # #
+    # param_dist = {"max_depth": [3, None],
+    #           "max_features": sp_randint(1, 11),
+    #           "min_samples_split": sp_randint(1, 11),
+    #           "min_samples_leaf": sp_randint(1, 11),
+    #           "bootstrap": [True, False],
+    #           "criterion": ["gini", "entropy"]}
+    #
+    # n_iter_search = 20
+    # random_search = RandomizedSearchCV(rndF, param_distributions=param_dist, n_iter=n_iter_search)
+    # random_search.fit(X,y)
+    # best_model = random_search.best_estimator_
+    # best_features = best_model.feature_importances_.argsort()[::-1]
+    # X_best = X[:,best_features[0:15]]
+    # X_train_best = X_train[:, best_features[0:15]]
+    # X_test_best = X_test[:, best_features[0:15]]
